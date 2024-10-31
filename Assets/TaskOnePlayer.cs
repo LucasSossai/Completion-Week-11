1cd582a44a7face7470f152c0b732a8fe44c28a2
LucasSossai <lucas@sossai.com>
2024-10-31 16:55:46 -0400

    Created Test Script
    just created to test debug logger and get familiar with github desktop

---
+++ Assets/TaskOnePlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class TaskOnePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Debug.Log("hello, this is a test.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
